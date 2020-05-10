ab776ce096d88c27cd0e9ce10519737ede5c8f30
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-05-09 22:27:29 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+import params from './src/params';
+import Field from './src/components/Field';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Iniciando o Mines!!!!</Text>
+        <Text>
+          Tamanho da Grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field opened mined />
+        <Field opened mined exploded />
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
